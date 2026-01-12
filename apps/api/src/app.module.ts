48b9843be6a11dd354bd9b35b2bdf7cb63faab55
cmvb <68516521+cm-verbose@users.noreply.github.com>
2026-01-11 23:12:45 -0500

    feat(db): basic user api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
-import AuthController from "./auth/auth.controller";
+import { AuthController } from "./auth/auth.controller";
 import PrismaService from "./prisma/prisma.service";
 import { AuthService } from "./auth/auth.service";
+import { JwtService } from "@nestjs/jwt";
 
-@Module({ imports: [], controllers: [AuthController], providers: [AuthService, PrismaService] })
+@Module({
+  imports: [],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService, JwtService],
+})
 export class AppModule {}
--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,13 +1,39 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, HttpCode, HttpStatus, Param, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { Prisma } from "@repo/db";
+import { AuthDTO } from "./auth.dto";
 
 @Controller("auth")
-export default class AuthController {
+export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post()
-  public async register(@Body() body: Prisma.UserCreateInput) {
+  /**
+   * Handles a user registration operation.
+   * @param body The informations used to register the new user.
+   * @returns
+   */
+  @Post("register")
+  public async register(@Body() body: AuthDTO) {
     return await this.authService.register(body);
+  }
+
+  /**
+   * Delete a given given user with it's id.
+   * @param id The id of the user to delete.
+   * @returns
+   */
+  @Delete(":id")
+  public async deleteUser(@Param("id") id: string) {
+    return await this.authService.deleteUser(id);
+  }
+
+  /**
+   * Handles a login operation.
+   * @param body The informations used to log the new user in.
+   * @returns
+   */
+  @Post("login")
+  @HttpCode(HttpStatus.OK)
+  public async login(@Body() body: AuthDTO) {
+    return await this.authService.login(body);
   }
 }
